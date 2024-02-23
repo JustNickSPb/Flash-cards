c13b35232ab1cec1fe0a959c95d81c06b8e51527
Nikita Barykin <mornind@gmail.com>
2024-02-23 17:35:35 +0300

    Добавил ендпойнты будущего приложения

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from blacksheep import Application, get
+from blacksheep import Application, Request, delete, get, post, put
 
 app = Application()
 
@@ -6,3 +6,28 @@ app = Application()
 @get("/")
 def home():
     return "Hello, World!"
+
+
+@get("/cards")
+def get_all_cards():
+    return "Тут будет ендпойнт, отдающий все карточки"
+
+
+@get("/cards/{id}")
+def get_card_by_id(id: int):
+    return f"Тут будет ендпойнт, отдающий карточку с id = {id}"
+
+
+@post("/cards")
+def add_card(request: Request):
+    return "Тут будет ендпойнт, добавляющий карточку"
+
+
+@put("/cards/{id}")
+def update_card(id: int, request: Request):
+    return f"Тут будет ендпойнт, обновляющий карточку с id = {id}"
+
+
+@delete("/cards/{id}")
+def delete_card(id: int):
+    return f"Тут будет ендпойнт, удаляющий карточку с id = {id}"
